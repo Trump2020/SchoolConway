6dc02700687dc8c771021326a4d0e6d45049cb7f
Samuel <252449@AHSE-K226-30>
2017-04-04 12:56:45 -0500

    Update drawcells.h

--- Conway/drawcells.h
+++ Conway/drawcells.h
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <Windows.h>
 
+double findcellcord(cells[x], cells[y]) {
+	
+}
 void drawcells() {
-
+	double findcellcord()
+	rectangle(cells[x][y], cells[x][y], cells[x][y] + scl, cells[x][y] + scl)
 }
