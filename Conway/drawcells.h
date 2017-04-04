9f44cafcb3694e6810ba36df7dc8ff6c7b826c08
Samuel <252449@AHSE-K226-30>
2017-04-04 12:38:15 -0500

    Add drawcells.h

---
+++ Conway/drawcells.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+
+void baseresize() {
+
+}
+void resizexplus() {
+	int *cell = new int[];
+}
+void resizeyplus() {
+
+}
+void resizexneg() {
+
+}
+void resizeyneg() {
+
+}
